e00031d33f111189150c50324cf1228bb35b346d
Jeremiah Limpin <it.jeremiahmlimpin@gmail.com>
2024-10-31 14:13:32 +0800

    update

--- teams-personal-detection.ps1
+++ teams-personal-detection.ps1
@@ -1,9 +1,9 @@
-#Script detects the new Microsoft Teams consumer app on Windows 11
+# Script detects the new Microsoft Teams consumer app on Windows 11
 
-if ($null -eq (Get-AppxPackage -Name MicrosoftTeams)) {
-    # Microsoft Teams client not found
+if ((Get-AppxPackage -Name MicrosoftTeams) -eq $null) {
+    # Microsoft Teams (personal) client not found
     exit 0
 } else {
-    # Microsoft Teams client found
+    # Microsoft Teams (personal) client found
     exit 1
 }
--- teams-personal-remediation.ps1
+++ teams-personal-remediation.ps1
@@ -1,9 +1,10 @@
-#Script removes the new Microsoft Teams consumer app on Windows 11
+# Script removes the new Microsoft Teams consumer app on Windows 11
 
-try{
-    Get-AppxPackage -Name MicrosoftTeams | Remove-AppxPackage -ErrorAction stop
-    # Microsoft Teams app successfully removed
-}catch{
-    # Errorremoving Microsoft Teams app
+try {
+    Get-AppxPackage -Name MicrosoftTeams | Remove-AppxPackage -ErrorAction Stop
+    # Microsoft Teams (personal) app successfully removed
+    exit 0
+} catch {
+    # Error removing Microsoft Teams (personal) app
     exit 2
 }
