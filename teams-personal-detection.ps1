6c712f88190ca1aa1d490f6fd6f1a850c78e1649
Jeremiah Limpin <it.jeremiahmlimpin@gmail.com>
2024-11-03 18:03:06 +0800

    update

--- teams-personal-detection.ps1
+++ teams-personal-detection.ps1
@@ -1,9 +1,8 @@
-# Script detects the new Microsoft Teams consumer app on Windows 11
-
+# Check if Microsoft Teams (personal) client is installed
 if ($null -eq (Get-AppxPackage -Name MicrosoftTeams)) {
-    # Microsoft Teams (personal) client not found
+    # Microsoft Teams (personal) client not found, exit with code 0
     exit 0
 } else {
-    # Microsoft Teams (personal) client found
+    # Microsoft Teams (personal) client found, exit with code 1
     exit 1
 }
