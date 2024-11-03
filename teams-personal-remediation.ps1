2ce9fba09c49e613cf013ef6be418603074077ff
Jeremiah Limpin <it.jeremiahmlimpin@gmail.com>
2024-11-03 18:06:38 +0800

    update

--- teams-personal-remediation.ps1
+++ teams-personal-remediation.ps1
@@ -1,9 +1,17 @@
-# Script removes the new Microsoft Teams consumer app on Windows 11
-
+# Script to remove the Microsoft Teams consumer app on Windows 11
 try {
-    Get-AppxPackage -Name MicrosoftTeams | Remove-AppxPackage -ErrorAction Stop
+    
+    # Check if the app is installed before attempting removal
+    $teamsApp = Get-AppxPackage -Name MicrosoftTeams -ErrorAction SilentlyContinue
+    if ($null -eq $teamsApp) {
+        # Microsoft Teams (personal) app not found. Nothing to remove
+        exit 0
+    }
+    # Remove the Teams app
+    $teamsApp | Remove-AppxPackage -ErrorAction Stop
     # Microsoft Teams (personal) app successfully removed
     exit 0
+
 } catch {
     # Error removing Microsoft Teams (personal) app
     exit 2
