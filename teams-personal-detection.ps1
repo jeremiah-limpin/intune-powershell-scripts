aedde1ab269876286b1db20fcdbfc9690a4f67b7
Jeremiah Limpin <it.jeremiahmlimpin@gmail.com>
2024-11-05 08:30:16 +0800

    alternate if-else

--- teams-personal-detection.ps1
+++ teams-personal-detection.ps1
@@ -1,9 +1,9 @@
 # Check if Microsoft Teams (personal) client is installed
-if ($null -eq (Get-AppxPackage -Name MicrosoftTeams -ErrorAction SilentlyContinue)) {
-    # Microsoft Teams (personal) client not found, exit with code 0
-    exit 0
-} else {
+if ((Get-AppxPackage -Name MicrosoftTeams -ErrorAction SilentlyContinue)) {
     # Microsoft Teams (personal) client found, exit with code 1
     exit 1
+} else {
+    # Microsoft Teams (personal) client not found, exit with code 0
+    exit 0
 }
 # This script is developed by Jeremiah Limpin https://github.com/jeremiah-limpin
